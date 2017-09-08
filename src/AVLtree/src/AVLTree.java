ed045cc8ecca925a6281d23f44106f561fee60de
yuri <yurijrdn@gmail.com>
2017-09-08 10:53:12 -0300

    Implementados os métodos de inserir, buscar e pré ordem

--- src/AVLtree/src/AVLTree.java
+++ src/AVLtree/src/AVLTree.java
@@ -1,22 +1,24 @@
+package AVLtree.src;
 
 import AVLtree.src.No;
-import java.util.List;
-
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Yuri
  */
-public class AVLTree implements interfaceAVL{
-    
+public class AVLTree {
+
     No root;
-    
+
+    public AVLTree() {
+        this.root = new No();
+    }
+
     public No getRoot() {
         return root;
     }
@@ -24,42 +26,94 @@ public class AVLTree implements interfaceAVL{
     public void setRoot(No root) {
         this.root = root;
     }
-    
-    
-    @Override
-    public No incluir(Object key) {
-        
-        No novo =  new No((int) key);
-        
-        if(this.root == null){ 
-            this.root = novo;
+
+    public void inserirNo(int chave, No root) {
+
+        No novo = new No(chave);
+
+        if (root == null) {
+            root = novo;
+        } else if (novo.getChave() < root.getChave()) {
+
+            if (root.getFilhoEsquerdo() == null) {
+
+                novo.setPai(root);
+                root.setFilhoEsquerdo(novo);
+                balancearAVL(root);
+
+            } else {
+                inserirNo(novo.getChave(), root.getFilhoEsquerdo());
+            }
+        } else if (novo.getChave() > root.getChave()) {
+
+            if (root.getFilhoDireito() == null) {
+
+                novo.setPai(root);
+                root.setFilhoDireito(novo);
+                balancearAVL(root);
+
+            } else {
+                inserirNo(novo.getChave(), root.getFilhoDireito());
+            }
+        } else {
+            System.out.println("Nó existente!");
         }
+    }
+
+    public No buscarNo(int chave, No root) {
+        
+        if(root == null) return null;
         else{
+            
+            if(root.getChave() < chave)
+                buscarNo(chave, root.getFilhoDireito());
+            
+            else if(root.getChave() > chave)
+                buscarNo(chave, root.getFilhoEsquerdo());
+        }
+        return root;
+    }
+
+    public boolean isEmpty(){ 
+        return this.root == null;
+    }
+    
+    public No remover(int chave) {
+        
+        No novo = buscarNo(chave, this.root);
+        
+        if(novo != null){
         }
         
+        return null;
     }
 
-    @Override
-    public boolean isEmpty() {
+    public void preOrdem(No root) {
+
+        if (root == null) return;
         
-    }
-
-    @Override
-    public No remover(Object key) {
+        System.out.println(root.getChave());
         
+        preOrdem(root.getFilhoEsquerdo());
+        
+        preOrdem(root.getFilhoDireito());
     }
     
- 
     
-    public List<No> inOrder(){};
+    public void rotEsquerdaSIMPLES() {
+    }
+
     
-    
-    public void rotEsquerdaSIMPLES(){};
-    public void rotDireitaSIMPLES(){};
-    
-    public void rotEsquerdaDUPLA(){};
-    public void rotDireitaDUPLA(){};
+    public void rotDireitaSIMPLES() {
+    }
 
     
     
+    public void rotEsquerdaDUPLA() {
+    }
+
+    
+    public void rotDireitaDUPLA() {
+    }
+
 }
